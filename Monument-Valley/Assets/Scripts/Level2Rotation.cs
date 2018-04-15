ee3c612ee11161c916164d0b4a3f2d45a1f94956
Chia-Yu Tang <37636434+JAlphalio@users.noreply.github.com>
2018-04-15 16:09:47 -0500

    Testing Level 2
    Left and Right to Spin
    Up and Down to move the platform

---
+++ Monument-Valley/Assets/Scripts/Level2Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Rotation : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("left"))
+        {
+            //rotatingClockwise = !rotatingClockwise;
+            transform.Rotate(0, -90, 0);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            //rotatingClockwise = !rotatingClockwise;
+            transform.Rotate(0, 90, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            transform.position = GameObject.FindGameObjectWithTag("Position2").transform.position;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            transform.position = GameObject.FindGameObjectWithTag("Position1").transform.position;
+        }
+    }
+}
