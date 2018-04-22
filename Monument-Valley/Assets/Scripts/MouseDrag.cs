ddc3e7787fe0141a300f5f674586f920fb1f500d
Chia-Yu Tang <37636434+JAlphalio@users.noreply.github.com>
2018-04-22 16:05:50 -0500

    Level 3 Unfinished
    There are two parts to level three, and it is the start of the whole story of the original monument valley. I can change it up a bit, but I stil need to finish it.

---
+++ Monument-Valley/Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour {
+
+    void OnMouseDrag()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition) + offset;
+
+        //if (transform.position.x >= 9.97 || transform.position.x <= 20.97)
+        //{
+            objPosition.y = transform.position.y;
+            objPosition.z = transform.position.z;
+            transform.position = objPosition;
+        //}
+    }
+}
