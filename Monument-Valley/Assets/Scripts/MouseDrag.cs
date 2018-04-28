3e7d409fe235e35d45a3b301ff1b08624324e604
Chia-Yu Tang <37636434+JAlphalio@users.noreply.github.com>
2018-04-27 21:08:27 -0500

    New script to draging

--- Monument-Valley/Assets/Scripts/MouseDrag.cs
+++ Monument-Valley/Assets/Scripts/MouseDrag.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class MouseDrag : MonoBehaviour {
 
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    public float yPoint;
+    public float zPoint;
+    public float xMinRange;
+    public float xMaxRange;
+    public int rangeLength;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
     void OnMouseDrag()
     {
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+       
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition) + offset;
-
-        //if (transform.position.x >= 9.97 || transform.position.x <= 20.97)
-        //{
-            objPosition.y = transform.position.y;
-            objPosition.z = transform.position.z;
-            transform.position = objPosition;
-        //}
+        //objPosition.y = transform.position.y;
+        //objPosition.z = transform.position.z;
+        if (objPosition.x >= xMinRange && objPosition.x <= xMaxRange)
+        {
+            objPosition.y = yPoint;
+            objPosition.z = zPoint;
+            /*for (int i = 1; i <= rangeLength+1; i++)
+            {
+                if (objPosition.x > (xMinRange + i - 2) && objPosition.x < xMinRange + i)
+                {
+                    objPosition.x = xMinRange + i;
+                }
+            }*/   
+            transform.position = objPosition;       
+        }
     }
 }
