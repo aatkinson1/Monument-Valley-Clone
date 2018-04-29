da33d3a21b7fc4b9977174ab9a4f101b8a4e2950
Chia-Yu Tang <37636434+JAlphalio@users.noreply.github.com>
2018-04-29 16:13:47 -0500

    I can't get the MenuScript to work

--- Monument-Valley/Assets/Scripts/MenuScript.cs
+++ Monument-Valley/Assets/Scripts/MenuScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MenuScript: MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
 	// Canvas
 	public Canvas mainMenu;
@@ -85,7 +85,7 @@ public class MenuScript: MonoBehaviour
 	public void ExitGame()
 	{
 		// Exit game
-		Application.Quit ();
+		Application.Quit();
 	}
 
 	// Function to load first level
