41019ff3734c2dc0371532eb386726478955a4cf
Nikki <nikkiatkinson1@gmail.com>
2018-05-01 22:33:13 -0500

    Added music and sound options
    Added a song to the game and have started working on the audio mute script. There are still some bugs to fix

---
+++ Monument-Valley/Assets/Scripts/Music.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    static Music instance = null;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ToggleSound()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+            // AudioListener.volume = 1;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            // AudioListener.volume = 0;
+        }
+    }
+}
---
+++ Monument-Valley/Assets/Scripts/Sound.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour {
+
+    private Music music;
+    public Button sound;
+    public Transform soundOFF;
+    public Transform soundON;
+
+	// Use this for initialization
+	void Start () {
+        music = GameObject.FindObjectOfType<Music>();
+        updateIcon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void pauseMusic()
+    {
+        music.ToggleSound();
+        updateIcon();
+    }
+
+    void updateIcon()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            if (soundON.gameObject.activeInHierarchy == false && soundOFF.gameObject.activeInHierarchy == true)
+            {
+                soundON.gameObject.SetActive(true);
+                soundOFF.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            if (soundOFF.gameObject.activeInHierarchy == false && soundON.gameObject.activeInHierarchy == true)
+            {
+                soundOFF.gameObject.SetActive(true);
+                soundON.gameObject.SetActive(false);
+            }
+        }
+    }
+}
