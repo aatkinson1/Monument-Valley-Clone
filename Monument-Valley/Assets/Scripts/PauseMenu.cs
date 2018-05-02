80f9c735e62d4a9d2b9512c85480e037e371f7b4
Ignacio Marroquin <ignacio9295@gmail.com>
2018-05-02 16:28:56 -0500

    Working on Pause Menu

---
+++ Monument-Valley/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour 
+{
+	public Transform PauseMenuCanvas;
+	public GameObject Player;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			Pause ();
+		}
+	}
+
+	public void Pause ()
+	{
+		if (PauseMenuCanvas.gameObject.activeInHierarchy == false) 
+		{
+			PauseMenuCanvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+			Player.GetComponent<SimpleCarController>().enabled = false;
+		} 
+		else 
+		{
+			PauseMenuCanvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+			Player.GetComponent<SimpleCarController>().enabled = true;
+		}
+	}
+
+	public void Exit ()
+	{
+		Application.Quit ();
+	}
+}
